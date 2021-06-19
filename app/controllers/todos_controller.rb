bab565e779cad820fd4da1e43e8b818dd2cf5fd7
utouto97 <utouto97@gmail.com>
2021-06-19 23:50:50 +0900

    add creation of new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,21 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def create
+    @todo = Todo.new(
+      title: params[:title],
+      description: params[:description],
+      status: 'In progress'
+    )
+
+    if @todo.save
+      redirect_to :action => 'index'
+    else
+      @todos = Todo.all
+      @title = params[:title]
+      @description = params[:description]
+      render 'index'
+    end
+  end
 end
